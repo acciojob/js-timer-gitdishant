831f174814761b0a34d221761f2285bba2f41c3d
gitdishant <92519667+gitdishant@users.noreply.github.com>
2024-09-18 22:37:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function updateTimer() {
+ function updateTimer() {
             const now = new Date();
-            const day = now.getDate();
-            const month = now.getMonth() + 1; // Months are zero-based
+            const day = String(now.getDate()).padStart(2, '0');
+            const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-based
             const year = now.getFullYear();
-            const hours = now.getHours();
-            const minutes = now.getMinutes();
-            const seconds = now.getSeconds();
+            const hours = String(now.getHours()).padStart(2, '0');
+            const minutes = String(now.getMinutes()).padStart(2, '0');
+            const seconds = String(now.getSeconds()).padStart(2, '0');
 
             const formattedTime = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
             document.getElementById('timer').textContent = formattedTime;
-	}
+        }
 
         setInterval(updateTimer, 1000); // Update the timer every second
-        updateTimer();
+        updateTimer(); 
